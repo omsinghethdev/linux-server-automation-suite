a1c3582df3b0f0b1728a1900e72e64c30be54ab4
om kumar singh <omsingh.ethdev@gmail.com>
2026-05-31 21:16:14 +0530

    Phase 1: Convert menu flow into reusable functions

--- main.sh
+++ main.sh
@@ -1,66 +1,85 @@
 #!/usr/bin/env bash
+show_menu(){
+	echo "==================================="
+        echo "  Linux Server Automation Suite    "
+        echo "==================================="
+
+        echo "1. User Management"
+        echo "2. System Monitoring"
+        echo "3. Backup Management"
+        echo "4. Log Analysis"
+        echo "5. Deployment Automation"
+        echo "6. Exit"
+        echo "==================================="
+
+	}
+get_valid_choice() {
+	attempt=0
+        maxattempt=3
+        
+        while (( attempt < maxattempt )); do
+                read -p "Enter your choice: " choice 
+                if [[ -z "${choice}" ]]; then
+                        echo "Choice is empty"
+                        echo "Choose Again"
+                        ((attempt++))
+
+
+                elif ! [[ "$choice" =~ ^[0-9]+$ ]]; then
+                        echo "Choice must be number."
+                        ((attempt++))
+
+
+                elif  (( choice <1 || choice > 6 )); then
+                        echo "Choice must be between 1 and 6"
+                        ((attempt++))
+
+                else 
+                        echo "Valid Choice"
+                        break
+                fi
+        
+                echo "Attempt left: $((maxattempt - attempt))"
+                        
+        done
+        if (( attempt == maxattempt )); then
+                echo "Too many invalid attempt. Exiting...."
+                exit 1
+        fi
+        
+}
+
+
+handle_choice() {
+		case "${choice}" in
+                1) echo "User Management selected" ;;
+                2) echo "System Monitoring selected" ;;
+                3) echo "Backup Management selected" ;;
+                4) echo "Log Analysis selected" ;;
+                5) echo "Deployment Automation selected" ;;
+                6) echo "Exiting..."; exit 0 ;;
+        	esac
+
+		}
+
+ask_continue() {
+		read -p  "Do you want to go to  Main menu?(yes/no):" confirm
+                    if [[ "${confirm}" == 'yes' ]]; then
+                        continue
+                    else 
+                        echo "Exiting Script...."
+                        exit 0
+                    fi
+
+		}
+
 
 while true; do
-	echo "==================================="
-	echo " 	Linux Server Automation Suite    "
-	echo "==================================="
-
-	echo "1. User Management"
-	echo "2. System Monitoring"
-	echo "3. Backup Management"
-	echo "4. Log Analysis"
-	echo "5. Deployment Automation"
-	echo "6. Exit"
-	echo "==================================="
+	show_menu
 	
-	attempt=0
-	maxattempt=3
-	
-	while (( attempt < maxattempt )); do
-		read -p "Enter your choice: " choice 
-		if [[ -z "${choice}" ]]; then
-			echo "Choice is empty"
-			echo "Choose Again"
-			((attempt++))
+	get_valid_choice
 
+	handle_choice
 
-		elif ! [[ "$choice" =~ ^[0-9]+$ ]]; then
-			echo "Choice must be number."
-			((attempt++))
-
-
-		elif  (( choice <1 || choice > 6 )); then
-			echo "Choice must be between 1 and 6"
-			((attempt++))
-
-		else 
-			echo "Valid Choice"
-			break
-		fi
-	
-		echo "Attempt left: $((maxattempt - attempt))"
-			
-	done
-	if (( attempt == maxattempt )); then
-		echo "Too many invalid attempt. Exiting...."
-		exit 1
-	fi
-	
-
-	case "${choice}" in
-    		1) echo "User Management selected" ;;
-   	        2) echo "System Monitoring selected" ;;
-    		3) echo "Backup Management selected" ;;
-      	        4) echo "Log Analysis selected" ;;
-    		5) echo "Deployment Automation selected" ;;
-    		6) echo "Exiting..."; exit 0 ;;
-	esac
-		read -p  "Do you want to go to  Main menu?(yes/no):" confirm
-		    if [[ "${confirm}" == 'yes' ]]; then
-			continue
-		    else 
-			echo "Exiting Script...."
-			exit 0
-		    fi
-	
+	ask_continue	
 done
